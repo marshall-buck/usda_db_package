81feaddfb3331afa18d2882b7ad69f6fa2a61e02
Marshall Buck <dev@marshallbuck.com>
2023-10-12 16:16:21 -0500

    added path package

--- lib/src/file_paths.dart
+++ lib/src/file_paths.dart
@@ -1,3 +1,9 @@
-const pathToTree = 'lib/assets/prefix_tree.json';
-const pathToWordIndex = 'lib/assets/db_word_index.json';
-const pathToFoods = 'lib/assets/db_food.json';
+import 'package:path/path.dart' as p;
+
+final pathToTree = p.join('lib', 'assets', 'prefix_tree.json');
+final pathToWordIndex = p.join('lib', 'assets', 'db_word_index.json');
+final pathToFoods = p.join('lib', 'assets', 'db_food.json');
+
+// const pathToTree = 'lib/assets/prefix_tree.json';
+// const pathToWordIndex = 'lib/assets/db_word_index.json';
+// const pathToFoods = 'lib/assets/db_food.json';
