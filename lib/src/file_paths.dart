4755d480520e1d4b22eac9610a8347c342f4a81f
Marshall Buck <dev@marshallbuck.com>
2023-11-03 16:09:54 -0500

    added json files for hash index and substrings

--- lib/src/file_paths.dart
+++ lib/src/file_paths.dart
@@ -3,6 +3,8 @@ import 'package:path/path.dart' as p;
 final pathToTree = p.join('assets', 'prefix_tree.json');
 final pathToWordIndex = p.join('assets', 'db_word_index.json');
 final pathToFoods = p.join('assets', 'db_food.json');
+final pathToSubstrings = p.join('assets', 'substrings.json');
+final pathToHashIndex = p.join('assets', 'hash_index.json');
 
 // const pathToTree = 'lib/assets/prefix_tree.json';
 // const pathToWordIndex = 'lib/assets/db_word_index.json';
