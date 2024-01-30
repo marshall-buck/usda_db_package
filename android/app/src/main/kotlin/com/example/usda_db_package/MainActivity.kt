25eaca74ffdee91edacbf90db1f871fe1fa8a63c
Marshall Buck <dev@marshallbuck.com>
2024-01-30 17:03:50 -0600

    Delete unused files and configurations

--- android/app/src/main/kotlin/com/example/usda_db_package/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.usda_db_package
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- test/src/string_helpers_test.dart
+++ test/src/string_helpers_test.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter_test/flutter_test.dart';
 import 'package:usda_db_package/src/string_helpers.dart';
 
