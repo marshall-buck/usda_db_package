4e746b31ab42c33a86e60b25e4b3a8589ba21c4d
Marshall Buck <dev@marshallbuck.com>
2024-02-08 15:32:33 -0600

    Add description comments to DescriptionRecord typedef

--- lib/src/type_def.dart
+++ lib/src/type_def.dart
@@ -1,2 +1,6 @@
 /// DescriptionRecord (description, description.length, food.id)
+///
+/// [description] is the description of the food.
+/// [description.length] is the length of the description.
+/// [food.id] is the id of the food.
 typedef DescriptionRecord = (String, int, int);
